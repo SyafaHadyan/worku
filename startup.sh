70afd6ce3b85fb5655aeb325decd0c8c2c1d4506
Syafa Hadyan Rasendriya <syafa@syafahadyan.com>
2026-03-02 22:03:14 +0700

    ci: build docker iamge

---
+++ startup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+printf "%s\n" "setting up environment variables"
+
+printf "%s\n" "" >.env
+
+printf "LIMITER_MAX=%s\n" $LIMITER_MAX >>.env
+printf "LIMITER_EXPIRATION_MINUTES=%s\n" $LIMITER_EXPIRATION_MINUTES >>.env
+printf "BODY_LIMIT_MB=%s\n" $BODY_LIMIT_MB >>.env
+
+printf "APP_PORT=%s\n" $APP_PORT >>.env
+
+printf "DB_NAME=%s\n" $DB_NAME >>.env
+printf "DB_USERNAME=%s\n" $DB_USERNAME >>.env
+printf "DB_PASSWORD=%s\n" $DB_PASSWORD >>.env
+printf "DB_HOST=%s\n" $DB_HOST >>.env
+printf "DB_PORT=%s\n" $DB_PORT >>.env
+
+printf "REDIS_ADDRESS=%s\n" $REDIS_ADDRESS >>.env
+printf "REDIS_PORT=%s\n" $REDIS_PORT >>.env
+printf "REDIS_USERNAME=%s\n" $REDIS_USERNAME >>.env
+printf "REDIS_PASSWORD=%s\n" $REDIS_PASSWORD >>.env
+printf "REDIS_DATABASE=%s\n" $REDIS_DATABASE >>.env
+printf "REDIS_EXPIRATION=%s\n" $REDIS_EXPIRATION >>.env
+
+printf "JWT_SECRET_KEY=%s\n" $JWT_SECRET_KEY >>.env
+printf "JWT_EXPIRED_DAYS=%s\n" $JWT_EXPIRED_DAYS >>.env
+
+printf "MIDTRANS_SERVER_KEY=%s\n" $MIDTRANS_SERVER_KEY >>.env
+
+printf "%s\n" "done setting up environment variables"
+printf "%s\n" "starting application"
+
+./main
